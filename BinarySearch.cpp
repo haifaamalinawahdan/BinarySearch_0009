dca6440d161d21a985bfcdac477e134b97510b93
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-16 15:01:29 +0700

    membuat perintah untuk memasukkan panjang elemen array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,3 +5,11 @@ int element[10];
 int nPanjang;
 int x;
 
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+    }
+}    
