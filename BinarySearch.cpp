032d284654340521a1a62defa7f14756d88b4201
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-16 15:45:03 +0700

    Menambahkan loop ke dalam void display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -59,4 +59,14 @@ void display()
     cout << "\n=======================================\n";
     cout << "\n Element Array Setelah Diurutkan(Asc)\n";
     cout << "\n=======================================\n";
+
+     for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
